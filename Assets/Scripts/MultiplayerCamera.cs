400888f5cee5a011654ea6a2f992c9e406c028f0
ShakeelMoh <shakmohamed1@hotmail.com>
2018-07-25 19:18:19 +0200

    Added Multiple Target Camera in scripts and modified the Camera prefab. Camera now follows both players

---
+++ Assets/Scripts/MultiplayerCamera.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//This script allows both players to be in view of the camera at all times
+public class MultiplayerCamera: MonoBehaviour {
+	public Transform Player1, Player2;
+	public float minSizeY = 5f;
+
+	void SetCameraPos() {
+		Vector3 middle = Player1.position + Player2.position * 0.5f;
+
+		gameObject.GetComponent<Camera>().transform.position = new Vector3(
+			gameObject.GetComponent<Camera>().transform.position.x,
+			middle.y,
+			middle.z
+		);
+	}
+
+	void SetCameraSize() {
+		//horizontal size is based on actual screen ratio
+		float minSizeX = minSizeY * Screen.width / Screen.height;
+
+		//multiplying by 0.5, because the ortographicSize is actually half the height
+		float width = Mathf.Abs(Player1.position.x - Player2.position.x) * 0.5f;
+		float height = Mathf.Abs(Player1.position.y - Player2.position.y) * 0.5f;
+
+		//computing the size
+		float camSizeX = Mathf.Max(width, minSizeX);
+		gameObject.GetComponent<Camera>().orthographicSize = Mathf.Max(height, camSizeX * Screen.height / Screen.width, minSizeY);
+	}
+
+	void Update() {
+		SetCameraPos();
+		SetCameraSize();
+	}
+}
---
+++ Assets/Scripts/MultipleTargetCamera.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Camera that keeps track of both players at the same time
+[RequireComponent(typeof(Camera))]
+public class MultipleTargetCamera : MonoBehaviour {
+
+	public List<Transform> targets;//Camera targets
+	public Vector3 offset;//Offset from players
+	private Vector3 velocity;//Stuff
+	public float smoothTime = .5f;
+	public float minZoom = 40f;//Field of view value
+	public float maxZoom = 10f;//Field of view value
+	public float zoomLimiter = 50f;
+
+	private Camera cam;
+
+	void Start(){
+		cam = GetComponent<Camera> ();
+	}
+
+	void LateUpdate(){
+
+		if (targets.Count == 0) {
+			return;
+		}
+
+		Move ();
+		Zoom ();
+	}
+
+	void Move(){
+
+		Vector3 centrePoint = GetCentrePoint ();
+		Vector3 newPosition = centrePoint + offset;
+		transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime); 
+
+	}
+
+	void Zoom(){
+		
+		float newZoom = Mathf.Lerp (maxZoom, minZoom, GetGreatestDistance ()/ zoomLimiter);
+		cam.fieldOfView = Mathf.Lerp (cam.fieldOfView, newZoom, Time.deltaTime);
+	}
+
+	float GetGreatestDistance(){
+		var bounds = new Bounds (targets [0].position, Vector3.zero);
+		for (int i = 0; i < targets.Count; i++) {
+			bounds.Encapsulate (targets [i].position);
+		}
+
+		return bounds.size.x;
+	}
+
+	//Obtains centre point
+	Vector3 GetCentrePoint(){
+
+		if (targets.Count == 1) {
+			return targets [0].position;
+		}
+
+		var bounds = new Bounds (targets [0].position, Vector3.zero);
+		for (int i = 0; i < targets.Count; i++) {
+			bounds.Encapsulate (targets [i].position);
+		}
+
+		return bounds.center;
+	}
+}
