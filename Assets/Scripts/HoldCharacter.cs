aad9e8d57d34f65c670ce95da59ec6ffc0bbb052
ShakeelMoh <shakmohamed1@hotmail.com>
2018-07-26 16:36:52 +0200

    Added moving platform to prefabs folder.

---
+++ Assets/Scripts/HoldCharacter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Used on moving platform
+//When object collides with platform, make platform the parent of the object
+public class HoldCharacter : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		other.transform.parent = gameObject.transform;
+	}
+
+	void OnTriggerExit(Collider other){
+		other.transform.parent = null;
+	}
+}
---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script moves the given platform between two positions
+public class MovingPlatform : MonoBehaviour {
+
+	public Transform movingPlatform;
+	public Transform position1;//startPos
+	public Transform position2;//endPos
+	public Vector3 newPositon;//Holds next pos
+	public string currentState;//Describes current state of platform
+
+	public float speed;//How long it takes to move platform
+	public float loopTime;//How long it waits at position
+
+	void Start () {
+		ChangeTarget ();
+	}
+	
+
+	void FixedUpdate () {//Fixed update runs at consistent interval. Update runs at fps interval.
+
+		movingPlatform.position = Vector3.Lerp (movingPlatform.position, newPositon, speed * Time.deltaTime);
+	}
+
+	void ChangeTarget(){
+
+		if (currentState == "Moving to position1") {
+			currentState = "Moving to position2";
+			newPositon = position2.position;
+
+		} else if (currentState == "Moving to position2") {
+			currentState = "Moving to position1";
+			newPositon = position1.position;
+
+		} else if (currentState == "") {
+			currentState = "Moving to position2";
+			newPositon = position2.position;
+		}
+
+		Invoke ("ChangeTarget", loopTime);
+
+	}
+}
