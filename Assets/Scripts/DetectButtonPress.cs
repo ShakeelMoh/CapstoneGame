5ba7e87078abf44af3f344b16343d494985eb408
ShakeelMoh <shakmohamed1@hotmail.com>
2018-07-27 12:36:38 +0200

    Added console. Press E or L to activate

---
+++ Assets/Scripts/DetectButtonPress.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Detects button press when within proximity to gameobject
+public class DetectButtonPress : MonoBehaviour {
+
+	public GameObject player1;
+	public GameObject player2;
+	private float distanceToOpen = 5;
+	private float distance;
+	private float distance2;
+	public AudioClip activateSound;
+	public AudioSource audioSource;
+	public float volume = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		distance = Vector3.Distance(transform.position, player1.transform.position);
+		distance2 = Vector3.Distance(transform.position, player2.transform.position);
+		if (distanceToOpen >= distance || distanceToOpen >= distance2) {
+
+			if (Input.GetKeyDown (KeyCode.E) || Input.GetKeyDown (KeyCode.L)) {
+				Debug.Log ("Button pressed");
+				audioSource.PlayOneShot (activateSound);
+			}
+
+		}
+		
+	}
+}
