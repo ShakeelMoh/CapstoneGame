4cec908e1720b026dcc1a73568f9e759b3a397b0
ShakeelMoh <shakmohamed1@hotmail.com>
2018-07-27 11:40:57 +0200

    Imported multiple free assets. Made jump pad and cctv camera prefabs

---
+++ Assets/Scripts/JumpPad.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour {
+
+	public int speed;
+
+	void OnCollisionEnter(Collision other){
+
+		Debug.Log ("Jump pad triggered");
+		other.gameObject.GetComponent<Rigidbody> ().AddForce (Vector3.up * speed);
+
+	}
+}
