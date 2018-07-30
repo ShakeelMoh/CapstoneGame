9dfb0ec9b28c6d906668fb55f94329a812dde004
ShakeelMoh <shakmohamed1@hotmail.com>
2018-07-30 19:43:52 +0200

    Portals now teleport but movement slightly weird

---
+++ Assets/Scripts/PortalTeleporter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Teleports player if they step through the portal
+public class PortalTeleporter : MonoBehaviour {
+
+	public GameObject otherPortal;
+	public float placementOffset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		other.transform.position = otherPortal.transform.position + otherPortal.transform.up * placementOffset;
+
+	}
+}
