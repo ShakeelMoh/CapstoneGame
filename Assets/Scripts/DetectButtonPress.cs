4829b971afe55a1a687f94e6c6c492bdf0001862
ShakeelMoh <shakmohamed1@hotmail.com>
2018-08-01 11:58:44 +0200

    Added console enabled portal prefab

--- Assets/Scripts/DetectButtonPress.cs
+++ Assets/Scripts/DetectButtonPress.cs
@@ -7,15 +7,34 @@ public class DetectButtonPress : MonoBehaviour {
 	public GameObject player1;
 	public GameObject player2;
 	private float distanceToOpen = 5;
+
 	private float distance;
 	private float distance2;
 	public AudioClip activateSound;
 	public AudioSource audioSource;
 	public float volume = 1.0f;
 
+	//Portal variables
+	public GameObject portal1;
+	public GameObject portal2;
+	private ParticleSystem psActivated1;
+	private ParticleSystem psActivated2;
+	private ParticleSystem psSmoke1;
+	private ParticleSystem psSmoke2;
+
 	// Use this for initialization
 	void Start () {
 		audioSource = GetComponent<AudioSource> ();
+
+		if (portal1 != null) {
+			psActivated1 = portal1.transform.Find ("Activated").GetComponent<ParticleSystem> ();
+			psSmoke1 = portal1.transform.Find ("Smoke").GetComponent<ParticleSystem> ();
+
+		}
+		if (portal2 != null) {
+			psActivated2 = portal2.transform.Find ("Activated").GetComponent<ParticleSystem> ();
+			psSmoke2 = portal2.transform.Find ("Smoke").GetComponent<ParticleSystem> ();
+		}
 	}
 	
 	// Update is called once per frame
@@ -28,6 +47,49 @@ public class DetectButtonPress : MonoBehaviour {
 			if (Input.GetKeyDown (KeyCode.E) || Input.GetKeyDown (KeyCode.L)) {
 				Debug.Log ("Button pressed");
 				audioSource.PlayOneShot (activateSound);
+
+				if (portal1 != null && portal2 != null) {
+					Debug.Log ("TRUE");
+					if (psActivated1.isStopped) {
+						psActivated1.Play ();
+					} else {
+						psActivated1.Stop ();
+					}
+					if (psActivated2.isStopped) {
+						psActivated2.Play ();
+					} else {
+						psActivated2.Stop ();
+					}
+
+					if (psSmoke1.isStopped) {
+						psSmoke1.Play ();
+					} else {
+						psSmoke1.Stop ();
+					}
+					if (psSmoke2.isStopped) {
+						psSmoke2.Play ();
+					} else {
+						psSmoke2.Stop ();
+					}
+					/*
+					psSmoke1.Play ();
+					psSmoke2.Play ();
+
+					if (psActivated1.isPlaying) {
+						psActivated1.Stop ();
+					}
+					if (psActivated2.isPlaying) {
+						psActivated2.Stop ();
+					}
+					if (psSmoke1.isPlaying) {
+						psSmoke1.Stop ();
+					}
+					if (psSmoke2.isPlaying) {
+						psSmoke2.Stop ();
+					}
+					*/
+				}
+
 			}
 
 		}
