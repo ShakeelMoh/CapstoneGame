a3358c7e2c2e1d5fd5786e9a01c34dc76b080ba1
ShakeelMoh <shakmohamed1@hotmail.com>
2018-08-22 23:39:59 +0200

    Added alarm lights

--- Assets/Scripts/Spin.cs
+++ Assets/Scripts/Spin.cs
@@ -8,6 +8,6 @@ public class Spin : MonoBehaviour
 
     void Update()
     {
-        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+		transform.Rotate(Vector3.right, speed * Time.deltaTime);
     }
 }
