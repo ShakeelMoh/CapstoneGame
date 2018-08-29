2368b7b17a04558d6368481a0606ce93fe2484d0
ShakeelMoh <shakmohamed1@hotmail.com>
2018-08-29 21:55:33 +0200

    Finished tile level

---
+++ Assets/Scripts/PictureChecker.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PictureChecker : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject tile1;
+	public GameObject tile2;
+	public GameObject tile3;
+	public GameObject tile4;
+	public GameObject tile5;
+	public GameObject tile6;
+	public GameObject tile7;
+	public GameObject tile8;
+	public GameObject tile9;
+	public GameObject tile10;
+	public GameObject tile11;
+	public GameObject tile12;
+	public GameObject tile13;
+	public GameObject tile14;
+	public GameObject tile15;
+
+	public GameObject door;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (tile1.GetComponent<PictureTile> ().correct) {
+			if (tile2.GetComponent<PictureTile> ().correct) {
+				if (tile3.GetComponent<PictureTile> ().correct) {
+					if (tile4.GetComponent<PictureTile> ().correct) {
+						if (tile5.GetComponent<PictureTile> ().correct) {
+							if (tile6.GetComponent<PictureTile> ().correct) {
+								if (tile7.GetComponent<PictureTile> ().correct) {
+									if (tile8.GetComponent<PictureTile> ().correct) {
+										if (tile9.GetComponent<PictureTile> ().correct) {
+											if (tile10.GetComponent<PictureTile> ().correct) {
+												if (tile11.GetComponent<PictureTile> ().correct) {
+													if (tile12.GetComponent<PictureTile> ().correct) {
+														if (tile13.GetComponent<PictureTile> ().correct) {
+															if (tile14.GetComponent<PictureTile> ().correct) {
+																if (tile15.GetComponent<PictureTile> ().correct) {
+																	door.GetComponent<OpenDoor> ().autoOpen = true;
+																} else {
+																	door.GetComponent<OpenDoor> ().autoOpen = false;
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PictureTile.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PictureTile : MonoBehaviour {
+
+
+	public Material[] materials;
+	public Material green;
+	public Material yellow;
+	public Material blue;
+	public Material red;
+	public Material white;
+	public Material magenta;
+	public Material black;
+	public Material currentColour;
+	public Material correctColour;
+
+	public int c = 0;
+	public GameObject otherTile;
+	public ParticleSystem floorGlow;
+
+	public bool correct;
+	// Use this for initialization
+	void Start () {
+		correct = false;
+		c = Random.Range (0, 7);
+
+		materials = new Material[]{red, blue, yellow, magenta, green, white, black};
+		floorGlow = this.transform.Find ("FloorGlow").GetComponent<ParticleSystem> ();
+		floorGlow.Stop ();
+		currentColour = white;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentColour == correctColour) {
+			correct = true;
+		} else {
+			correct = false;
+		}
+	}
+
+	void OnCollisionEnter(Collision other){
+				
+		this.GetComponent<Renderer> ().material = materials [c];
+		currentColour = materials [c];
+		//floorGlow.Play ();
+		otherTile.GetComponent<Renderer> ().material = materials[c];
+		if (c < materials.Length - 1) {
+			c++;
+		} else {
+			c = 0;
+		}
+
+	}
+
+	IEnumerator doSomething(float wait){
+		yield return new WaitForSeconds (wait);
+
+	}
+
+	void OnCollisionExit(Collision other){
+		if (floorGlow.isPlaying) {
+			floorGlow.Stop ();
+		}
+	}
+}
