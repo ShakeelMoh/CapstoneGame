5e72a49c7e6272522f97c6091c5505077697fbfc
Moegamat Ra-eez Stenekamp <eezy1997@gmail.com>
2018-10-12 19:38:44 +0200

    lvl 9 changes.

---
+++ Assets/Scripts/CreateInvisWall.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateInvisWall : MonoBehaviour {
+    
+    private void OnTriggerEnter(Collider col)
+    {
+        GetComponent<Collider>().isTrigger = false;
+    }
+}
---
+++ Assets/Scripts/FallingObjectDestroyer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//falling objects that hit this get destroyed, and if its a player they get respawned
+
+public class FallingObjectDestroyer : MonoBehaviour {
+
+    public GameObject levelTracker; //To keep track of current level
+    public GameObject levelSpawnAreas;
+    public GameObject Player1;
+    public GameObject Player2;
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            int level = levelTracker.gameObject.GetComponent<LevelTracker>().level;
+            transform.position = levelSpawnAreas.transform.Find(level + "").position;
+        }
+        else
+            Destroy(col.gameObject);
+    }
+}
