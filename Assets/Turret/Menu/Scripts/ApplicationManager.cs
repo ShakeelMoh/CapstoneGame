b4d8840ec6aa4d3533ed37b786483dccd392a193
Tony Guo <gxxhen001@myuct.ac.za>
2018-10-15 18:11:58 +0200

    Polished Menus.

--- Assets/Turret/Menu/Scripts/ApplicationManager.cs
+++ Assets/Turret/Menu/Scripts/ApplicationManager.cs
@@ -1,21 +1,56 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using System.Collections;
 
 public class ApplicationManager : MonoBehaviour {
-	
+    //public Slider slider;
+    //public Text progressText;
+    public Button playButton;
 
-	public void Quit () 
+    /*bool bPlay;
+    AsyncOperation operation;*/
+
+    private void Start()
+    {
+        EventSystem.current.SetSelectedGameObject(playButton.gameObject);
+    }
+
+    public void Quit () 
 	{
-		#if UNITY_EDITOR
+        #if UNITY_EDITOR
 		UnityEditor.EditorApplication.isPlaying = false;
-		#else
+        #else
 		Application.Quit();
-		#endif
+        #endif
 	}
 
-    public void Play(string screenName)
+    public void Play()
     {
-        SceneManager.LoadScene(screenName);
+        SceneManager.LoadScene("PortalBalls");
+
     }
+
+    /*IEnumerator LoadAsynchronously(string screenName)
+    {
+        operation = SceneManager.LoadSceneAsync(screenName);
+        operation.allowSceneActivation = false;
+        while (!operation.isDone)
+        {
+            if (operation.progress < 0.9f)
+            {
+                float progress = Mathf.Clamp01(operation.progress / 0.9f);
+                slider.value = progress;
+                progressText.text = progress * 100f + "%";
+            }
+            else
+            {
+                playButton.interactable = true;
+            }
+            
+        }
+        yield return null;
+    }*/
+
 }
