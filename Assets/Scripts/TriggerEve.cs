29370d1b2964aeb9e573fc081913881d6a3a527d
Shakeel <shakmohamed1@hotmail.com>
2018-10-15 18:19:36 +0200

    Added end game trigger

---
+++ Assets/Scripts/TriggerEve.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class TriggerEve : MonoBehaviour {
+
+    public GameObject eve;
+    public GameObject target;
+    public GameObject target2;
+    public int waitTime;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator OnTriggerEnter(Collider other)
+    {
+        yield return new WaitForSeconds(waitTime);
+        eve.gameObject.GetComponent<AICharacterControl>().target = target.transform;
+        eve.gameObject.transform.LookAt(target2.transform);
+    }
+}
